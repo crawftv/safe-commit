dfb8f089ef62c73205e89b9a868d3f924d27f519
Crawford Collins <crawford.collins@gmail.com>
2020-11-06 23:21:51 -0600

    Creation (#3)
    * tox works with changes to setup.py, added a full .pre-commit-config.yaml to project
    
    * cleaned up test file

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,43 +1,27 @@
 import pytest
 import os
 
-def test_success(tmpdir):
-    os.chdir(tmpdir)
-    os.system("git init")
-    os.system("git checkout -b success ")
-    with open(".pre-commit-config.yaml","w") as file:
-        file.write(
-            "repos:\n"
-            "-   repo: local\n"
-            "    hooks:\n"
-            "    -   id: safe-commit\n"
-            "        name: safe-commit\n"
-            "        entry: safe-commit\n"
-            "        language: system\n"
-            "        pass_filenames: false\n"
-            "        always_run: true\n"
-        )
-    assert os.system("pre-commit install") == 0
-    os.system("git add .pre-commit-config.yaml")
-    os.system("git commit -m 'test' ")
 
-def test_failure(tmpdir):
+@pytest.mark.parametrize(
+    "branch,solution,protected_branches",
+    [("success", 0, ""), ("success", 1, "success"), ("main", 1, ""), ("master", 1, "")],
+)
+def test_success(tmpdir, branch, solution, protected_branches):
     os.chdir(tmpdir)
     os.system("git init")
-    os.system("git checkout -b failure ")
-    with open(".pre-commit-config.yaml","w") as file:
+    os.system(f"git checkout -b {branch}")
+    with open(".pre-commit-config.yaml", "w") as file:
         file.write(
             "repos:\n"
             "-   repo: local\n"
             "    hooks:\n"
             "    -   id: safe-commit\n"
             "        name: safe-commit\n"
-            "        entry: safe-commit --protected_branches failure\n"
+            f"        entry: safe-commit {protected_branches}\n"
             "        language: system\n"
             "        pass_filenames: false\n"
             "        always_run: true\n"
         )
     os.system("pre-commit install")
-    os.system("safe-commit --protected_branches failure")
     os.system("git add .pre-commit-config.yaml")
-    os.system("git commit -m 'test' ")
+    assert os.system("git commit -m 'test' ") == solution
