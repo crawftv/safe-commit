084426412ef80f277de0f7dbead356d3b66a77df
crawford <crawford.collins@gmail.com>
2020-11-06 21:56:19 -0600

    pytest works

--- tests/test_main.py
+++ tests/test_main.py
@@ -17,14 +17,14 @@ def test_success(tmpdir):
             "        pass_filenames: false\n"
             "        always_run: true\n"
         )
-    os.system("pre-commit install")
+    assert os.system("pre-commit install") == 0
     os.system("git add .pre-commit-config.yaml")
-    assert os.system("git commit -m 'test success' ")
+    os.system("git commit -m 'test' ")
 
 def test_failure(tmpdir):
     os.chdir(tmpdir)
     os.system("git init")
-    #os.system("git checkout -b success ")
+    os.system("git checkout -b failure ")
     with open(".pre-commit-config.yaml","w") as file:
         file.write(
             "repos:\n"
@@ -32,11 +32,12 @@ def test_failure(tmpdir):
             "    hooks:\n"
             "    -   id: safe-commit\n"
             "        name: safe-commit\n"
-            "        entry: safe-commit\n"
+            "        entry: safe-commit --protected_branches failure\n"
             "        language: system\n"
             "        pass_filenames: false\n"
             "        always_run: true\n"
         )
     os.system("pre-commit install")
+    os.system("safe-commit --protected_branches failure")
     os.system("git add .pre-commit-config.yaml")
-    assert os.system("git commit -m 'test success' ")
+    os.system("git commit -m 'test' ")
