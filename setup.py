308302388fbb6696f9bf74113848428155bd6513
crawford <crawford.collins@gmail.com>
2020-11-12 19:27:14 -0600

    added pre-commit-hooks file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="safe-commit",
     author="Crawford Collins",
     description="A pre-commit hook for stopping you from committing to the main branch.",
-    version="1.0.0",
+    version="1.0.1",
     packages=["src"],
     install_requires=["gitpython"],
     entry_points={"console_scripts": ["safe-commit=src.main:main"]},
